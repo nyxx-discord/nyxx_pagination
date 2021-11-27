a7df45bad58b9d2c85192d9d77e6f71757e7358a
Szymon Uglis <szymon@l7ssha.xyz>
2021-11-27 22:52:07 +0100

    Release: 2.0.0-dev.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 2.0.0-dev.0
+
+- Bump `nyxx` and `nyxx_interactions`  to `3.0.0-dev.0`
+- Fixup names of classes
+- Migrate to export/import model
+
 ## 1.0.1
 
 - Push fixup
--- example/example.dart
+++ example/example.dart
@@ -1,10 +1,10 @@
 import "dart:async";
 
 import "package:nyxx/nyxx.dart";
-import "package:nyxx_interactions/interactions.dart";
+import 'package:nyxx_interactions/nyxx_interactions.dart';
 import "package:nyxx_pagination/nyxx_pagination.dart";
 
-FutureOr<void> paginationExampleInteraction(SlashCommandInteractionEvent event) {
+FutureOr<void> paginationExampleInteraction(ISlashCommandInteractionEvent event) {
   final paginator = EmbedComponentPagination(event.interactions, [
     EmbedBuilder()..description = "This is first page",
     EmbedBuilder()..description = "This is second page",
@@ -14,9 +14,9 @@ FutureOr<void> paginationExampleInteraction(SlashCommandInteractionEvent event) 
 }
 
 void main() {
-  final bot = Nyxx("<TOKEN>", GatewayIntents.allUnprivileged);
+  final bot = NyxxFactory.createNyxxWebsocket("<TOKEN>", GatewayIntents.allUnprivileged);
 
-  final interaction = Interactions(bot)
+  IInteractions.create(WebsocketInteractionBackend(bot))
     ..registerSlashCommand(SlashCommandBuilder("paginated", "This is pagination example", [], guild: 302360552993456135.toSnowflake())
       ..registerHandler(paginationExampleInteraction))
     ..syncOnReady();
