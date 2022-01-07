d202cdf6bd14b4483c05ff9f39eeb30534fc0e32
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2022-01-07 15:23:42 +0100

    Allow custom emojis and labels

--- lib/src/component_pagination.dart
+++ lib/src/component_pagination.dart
@@ -13,6 +13,16 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
   /// Custom id for this instance of paginator that different paginators could be recognized.
   late final String customPreId;
 
+  final String firstLabel;
+  final String prevLabel;
+  final String nextLabel;
+  final String lastLabel;
+
+  final IEmoji? firstEmoji;
+  final IEmoji? prevEmoji;
+  final IEmoji? nextEmoji;
+  final IEmoji? lastEmoji;
+
   /// Current page that paginator is on
   @override
   int currentPage = 1;
@@ -21,7 +31,17 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
   late ComponentMessageBuilder builder;
 
   /// Creates new paginator using interactions
-  ComponentPaginationAbstract(this.interactions) {
+  ComponentPaginationAbstract(
+    this.interactions, {
+    this.firstLabel = '<<',
+    this.prevLabel = '<',
+    this.nextLabel = '>',
+    this.lastLabel = '>>',
+    this.firstEmoji,
+    this.prevEmoji,
+    this.nextEmoji,
+    this.lastEmoji,
+  }) {
     customPreId = randomAlpha(10);
   }
 
@@ -29,7 +49,7 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
   @override
   ComponentMessageBuilder initMessageBuilder() {
     final firstPageButtonId = "${customPreId}firstPage";
-    final firstPageButton = ButtonBuilder("<<", firstPageButtonId, ComponentStyle.secondary);
+    final firstPageButton = ButtonBuilder(firstLabel, firstPageButtonId, ComponentStyle.secondary, emoji: firstEmoji);
     interactions.events.onButtonEvent.where((event) => event.interaction.customId == firstPageButtonId).listen((event) async {
       await event.acknowledge();
 
@@ -38,7 +58,7 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
     });
 
     final previousPageButtonId = "${customPreId}previousPage";
-    final previousPageButton = ButtonBuilder("<", previousPageButtonId, ComponentStyle.secondary);
+    final previousPageButton = ButtonBuilder(prevLabel, previousPageButtonId, ComponentStyle.secondary, emoji: prevEmoji);
     interactions.events.onButtonEvent.where((event) => event.interaction.customId == previousPageButtonId).listen((event) async {
       await event.acknowledge();
 
@@ -47,7 +67,7 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
     });
 
     final nextPageButtonId = "${customPreId}nextPage";
-    final nextPageButton = ButtonBuilder(">", nextPageButtonId, ComponentStyle.secondary);
+    final nextPageButton = ButtonBuilder(nextLabel, nextPageButtonId, ComponentStyle.secondary, emoji: nextEmoji);
     interactions.events.onButtonEvent.where((event) => event.interaction.customId == nextPageButtonId).listen((event) async {
       await event.acknowledge();
 
@@ -56,7 +76,7 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
     });
 
     final lastPageButtonId = "${customPreId}lastPage";
-    final lastPageButton = ButtonBuilder(">>", lastPageButtonId, ComponentStyle.secondary);
+    final lastPageButton = ButtonBuilder(lastLabel, lastPageButtonId, ComponentStyle.secondary, emoji: lastEmoji);
     interactions.events.onButtonEvent.where((event) => event.interaction.customId == lastPageButtonId).listen((event) async {
       await event.acknowledge();
 
@@ -103,7 +123,27 @@ abstract class ComponentPaginationAbstract extends IPagination<IButtonInteractio
 /// [getMessageBuilderForPage] needs to be implemented in order to work.
 abstract class ComponentPaginationBase extends ComponentPaginationAbstract {
   /// Creates instance of [ComponentPaginationBase]
-  ComponentPaginationBase(IInteractions interactions) : super(interactions);
+  ComponentPaginationBase(
+    IInteractions interactions, {
+    String firstLabel = '<<',
+    String prevLabel = '<',
+    String nextLabel = '>',
+    String lastLabel = '>>',
+    IEmoji? firstEmoji,
+    IEmoji? prevEmoji,
+    IEmoji? nextEmoji,
+    IEmoji? lastEmoji,
+  }) : super(
+          interactions,
+          firstLabel: firstLabel,
+          prevLabel: prevLabel,
+          nextLabel: nextLabel,
+          lastLabel: lastLabel,
+          firstEmoji: firstEmoji,
+          prevEmoji: prevEmoji,
+          nextEmoji: nextEmoji,
+          lastEmoji: lastEmoji,
+        );
 
   @override
   FutureOr<void> updatePage(int page, ComponentMessageBuilder currentBuilder, IButtonInteractionEvent target) {
@@ -120,7 +160,28 @@ class EmbedComponentPagination extends ComponentPaginationBase {
   final List<EmbedBuilder> embeds;
 
   /// Creates instance of [EmbedComponentPagination]
-  EmbedComponentPagination(IInteractions interactions, this.embeds) : super(interactions);
+  EmbedComponentPagination(
+    IInteractions interactions,
+    this.embeds, {
+    String firstLabel = '<<',
+    String prevLabel = '<',
+    String nextLabel = '>',
+    String lastLabel = '>>',
+    IEmoji? firstEmoji,
+    IEmoji? prevEmoji,
+    IEmoji? nextEmoji,
+    IEmoji? lastEmoji,
+  }) : super(
+          interactions,
+          firstLabel: firstLabel,
+          prevLabel: prevLabel,
+          nextLabel: nextLabel,
+          lastLabel: lastLabel,
+          firstEmoji: firstEmoji,
+          prevEmoji: prevEmoji,
+          nextEmoji: nextEmoji,
+          lastEmoji: lastEmoji,
+        );
 
   @override
   ComponentMessageBuilder getMessageBuilderForPage(int page, ComponentMessageBuilder currentBuilder) => currentBuilder..embeds = [embeds[page - 1]];
@@ -135,7 +196,28 @@ class SimpleComponentPagination extends ComponentPaginationBase {
   final List<String> contentPages;
 
   /// Creates instance of [SimpleComponentPagination]
-  SimpleComponentPagination(IInteractions interactions, this.contentPages) : super(interactions);
+  SimpleComponentPagination(
+    IInteractions interactions,
+    this.contentPages, {
+    String firstLabel = '<<',
+    String prevLabel = '<',
+    String nextLabel = '>',
+    String lastLabel = '>>',
+    IEmoji? firstEmoji,
+    IEmoji? prevEmoji,
+    IEmoji? nextEmoji,
+    IEmoji? lastEmoji,
+  }) : super(
+          interactions,
+          firstLabel: firstLabel,
+          prevLabel: prevLabel,
+          nextLabel: nextLabel,
+          lastLabel: lastLabel,
+          firstEmoji: firstEmoji,
+          prevEmoji: prevEmoji,
+          nextEmoji: nextEmoji,
+          lastEmoji: lastEmoji,
+        );
 
   @override
   ComponentMessageBuilder getMessageBuilderForPage(int page, ComponentMessageBuilder currentBuilder) => currentBuilder..content = contentPages[page - 1];
